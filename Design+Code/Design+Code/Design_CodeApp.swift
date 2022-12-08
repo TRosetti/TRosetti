b01799f87843a72daa74473c4142babc68031a51
TRosetti <105075815+TRosetti@users.noreply.github.com>
2022-12-08 13:41:53 -0300

    Initial commit

---
+++ Design+Code/Design+Code/Design_CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_CodeApp.swift
+//  Design+Code
+//
+//  Created by Thiago Rosetti Miranda on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Design_CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TRosetti
+ SwiftUi
